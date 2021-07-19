2a9be57389ba51a72771f4e7de0a406579e13966
Mrinmoy Kumar Biswas <mrinmoy.blr@gmail.com>
2021-07-19 13:35:01 +0530

    First Commit 001

---
+++ src/main/java/Basics.java
@@ -0,0 +1,16 @@
+
+public class Basics {
+
+	public static void main(String[] args) {
+		// Validate is the Add Place API is working fine
+		// Rest Assured will work on the below mentioned 3 principals
+		//given: All the input details 
+		//when: Submit the API
+		//then: Validate the Response
+		
+		
+		
+
+	}
+
+}
