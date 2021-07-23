3f75a82eeacc49b57e2f4c037e316f096d04fe81
Mrinmoy Kumar Biswas <mrinmoy.blr@gmail.com>
2021-07-23 11:04:44 +0530

    First Commit 001

---
+++ src/main/java/restAssured/oAuthTest.java
@@ -0,0 +1,23 @@
+package restAssured;
+
+import static io.restassured.RestAssured.*;
+import static org.hamcrest.Matchers.*;
+import io.restassured.path.json.JsonPath;
+
+public class oAuthTest {
+	public static void main(String[] args) {
+		String accessTokenResponse = given().queryParam("code", "")
+				.queryParam("client_id", "692183103107-p0m7ent2hk7suguv4vq22hjcfhcr43pj.apps.googleusercontent.com")
+				.queryParam("client_secret", "erZOWM9g3UtwNRj340YYaK_W")
+				.queryParam("redirect_uri", "https://rahulshettyacademy.com/getCourse.php")
+				.queryParam("grant_type", "authorization_code").when().log().all()
+				.post("https://www.googleapis.com/oauth2/v4/token").asString();
+		JsonPath js = new JsonPath(accessTokenResponse);
+		String accesToken = js.getString("access_token");
+
+		String response = given().queryParam("access_token", accesToken).when().log().all()
+				.get("https://rahulshettyacademy.com/getCourse.php").asString();
+		System.out.println(response);
+		JsonPath js = new JsonPath(response);
+	}
+}
