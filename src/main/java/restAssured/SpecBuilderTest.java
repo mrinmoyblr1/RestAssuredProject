70d7703d9d18da4a3783dbbc27f48f65a22215da
Mrinmoy Kumar Biswas <mrinmoy.blr@gmail.com>
2021-08-02 16:49:57 +0530

    DSD

--- src/main/java/restAssured/SpecBuilderTest.java
+++ src/main/java/restAssured/SpecBuilderTest.java
@@ -13,5 +13,43 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class SpecBuilderTest {
-	public static void main(String[] args) {}
+	public static void main(String[] args) {
+		// RestAssured.baseURI = "https://rahulshettyacademy.com";
+		AddPlace p = new AddPlace();
+		Location l = new Location();
+		p.setAccuracy(50);
+		p.setAddress("29, side layout, cohen 09");
+		p.setLanguage("French-IN");
+		p.setPhone_number("+91) 983 893 3937");
+		p.setWebsite("http://google.com");
+		p.setName("Frontline house");
+		List<String> myList = new ArrayList<String>();
+		myList.add("shoe park");
+		myList.add("shop");
+		p.setTypes(myList);
+		l.setLat(-38.383494);
+		l.setLng(33.427362);
+		p.setLocation(l);
+		
+		
+		RequestSpecification req = new RequestSpecBuilder().setBaseUri("https://rahulshettyacademy.com")
+				.addQueryParam("Key", "qaclick123").setContentType(ContentType.JSON).build();
+		
+		
+		ResponseSpecification resSpec = new ResponseSpecBuilder().expectStatusCode(200)
+				.expectContentType(ContentType.JSON).build();
+		
+
+
+		RequestSpecification res = given().spec(req).body(p).log().all();
+
+		Response response = res.when().post("/maps/api/place/add/json").then().spec(resSpec).log().all().extract().response();
+
+
+		Response response1 = res.when().post("/maps/api/place/add/json").then().spec(resSpec).extract().response();
+		
+
+		String responseString = response.asString();
+		System.out.println(responseString);		
+	}
 }
